8c6034f9fd40a19c63737e1bab78017c954a4e62
Ankit Raj <rajankit840968@gmail.com>
2025-04-11 17:55:30 +0530

    add: create asynchronous JavaScript examples with color changing and timeout functionality

---
+++ JavaScript/15-AsyncJavascript/asynchronus.js
@@ -0,0 +1,6 @@
+//javascript is always "single threaded" and "synchronous"
+//js engine need browser or node environment to run...
+//js engine is itself single threaded only
+//"task queue" makes js fast and asynchronous
+//for "promises" there is high priority queue or promise queue
+//WEB API consists of DOM API, setTimeout, setInterval, fetch(), Register Call Back
