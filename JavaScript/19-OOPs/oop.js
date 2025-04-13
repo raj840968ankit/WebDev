2b2f61c1a77e07776ba18eb008f874d880f03a17
Ankit Raj <rajankit840968@gmail.com>
2025-04-13 22:29:23 +0530

    add: implement oop.js and prototype.js to demonstrate JavaScript OOP concepts and prototypal behavior

---
+++ JavaScript/19-OOPs/oop.js
@@ -0,0 +1,40 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+//we will create object of User after creating it as a constructor function using new keyword
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+//using the 'new' keyword a constructor function has been called
+//here we are using new to create a constructor function object that will return object
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
---
+++ JavaScript/19-OOPs/prototype.js
@@ -0,0 +1,47 @@
+//javascript shows prototypal behavior
+//(functions, strings and array) all are prototype of object
+function multipleBy5(num){
+
+    return num*5
+}
+
+multipleBy5.power = 2
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);  // it will print empty object({}) because there is no object in array
+
+
+//creating a constructor function and associating prototype function with it.................
+//Remember - prototype can be only used with constructor function or Object and Array class
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+createUser.prototype.printMe = function(){   //this will add a property 'printMe' to the createUser's Object
+    console.log(`my name is ${this.username} and score is ${this.score}`);
+}
+const ankit = new createUser("ankit", 100)
+const rahul = new createUser("rahul", 200)
+ankit.printMe()
+
+
+const heroes = ['thor', 'spiderman']
+const power = {
+    thor : 'thunder god',
+    spiderman : 'web',
+    getSpidermanPower : function(){
+        console.log(`spidy power is ${this.spiderman}`);
+    }
+}
+Object.prototype.ankit = function(){ //this will add a property to Object class
+    console.log('Ankit is present in all object')
+}
+power.ankit()
+
+//adding a true length method to find trueLength of string
+String.prototype.trueLength = function(){
+    return `${this.trim().length}`
+}
+const name = '    ankit    '
+console.log(name.trueLength());
