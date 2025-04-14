5fe0047db6cede574e59b73b4e4d73870552d6fb
ankit <rajankit840968@gmail.com>
2025-04-15 03:38:10 +0530

    add: create command.js to document common Git commands and usage

---
+++ GitCommand/command.js
@@ -0,0 +1,29 @@
+//for configuring username and email.....
+//   git config --global user.name "username"
+//   git config --global user.email "userEmail"
+
+
+//for cloning github repositories to vs code new folder.....
+//   navigate to that folder in terminal
+//   git clone repository link(link will be provided in github website code -> https section)
+
+
+//for tracking status of file
+//   git status (untracked, unmodified, modified, staged)
+//   untracked - new files
+//   unmodified - no modification in exiting file
+//   modified - modification in existing file
+//   staged - add modified files 
+
+
+//to add a file
+//   git add filename
+//   git add . (to add all file at once)
+
+
+//to commit the files
+//   git commit -m "commit message" (it will be committed for local machines)
+
+
+//to push on github
+//   git push origin main
