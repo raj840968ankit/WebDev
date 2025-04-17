96973be7272729f3723028348a26648bca688aa4
ankit <rajankit840968@gmail.com>
2025-04-17 23:55:43 +0530

    feat: expand Node.js theory documentation with additional details on built-in modules and package management

--- NodeJS/1-Theory/theory.js
+++ NodeJS/1-Theory/theory.js
@@ -1,3 +1,9 @@
 //Node js is an open source, server-side Javascript runtime
 //it is platform independent
 //Node js provide runtime environment to run JS code on server side instead of browser
+//Node js come with built-in modules like 'http' which made it popular for creating backend application
+
+//NPM is introduced to manage the packages of node js
+//FNM(fast node manager) is a command line utility that enable us to switch on any node.js version
+
+///
