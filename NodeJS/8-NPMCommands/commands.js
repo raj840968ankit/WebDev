57e55b98e563a186f93efc5a6582144110b28057
ankit <rajankit840968@gmail.com>
2025-04-25 02:22:36 +0530

    feat: add npm command examples for package management

---
+++ NodeJS/8-NPMCommands/commands.js
@@ -0,0 +1,4 @@
+// npm install "packageName" -> for installing a package
+// npm install -> if you deleted node_modules directory then it will restore
+// npm list -> shows all dependencies of your project
+// npm view 'packageName' -> show details about packages
