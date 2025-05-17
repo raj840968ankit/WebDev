d7f80533c3d62b18df346fae48499e99b4b467a0
ankit <rajankit840968@gmail.com>
2025-05-18 01:06:53 +0530

    feat: add initial ExpressJS server setup with routing and middleware comments

---
+++ ExpressJS/1.Theory/theory.js
@@ -0,0 +1,4 @@
+//express js is a flexible framework of node js based on http module
+//it simplifies routing, middleware and handling Http request in node js
+
+//install via "npm i express"
---
+++ ExpressJS/2.CreatingServer/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+//creating an instance of express
+const app = express()
+//"app" variable holds the created express app which i can use for :-
+//   1.Define routes (aap.get(), app.post(), etc)
+//   2.Configure middleware (app.use())
+//   3.Start the server (app.listen())
+
+//...............creating server......................
+app.get('/', (req, res) => {  //here get method is used to send response from server
+    return res.send("<h1>Hello World!!</h1>")
+})
+
+app.get('/about', (req, res) => {  //here get method is used to send response from server
+    return res.send("<h1>Hello About page!!</h1>")
+})
+
+const port = 3000
+app.listen(port, () =>{
+    console.log(`Server is listening at http://localhost:${port}.....`);
+})
+
+
