e0bbb0e69577d2629cedab509894c3a8e806cbbf
ankit <rajankit840968@gmail.com>
2025-05-19 00:39:32 +0530

    feat: initialize Express app with dotenv configuration and add Tic Tac Toe game files

---
+++ ExpressJS/4.SendFiles/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'   //for using env file
+import path from 'path'
+
+dotenv.config()
+
+const app = express()
+
+const staticPath = path.join(import.meta.dirname, "public")
+app.use(express.static(staticPath))  //middleware of express - used to send static files (e.g. html, css, js etc)
+
+app.get('/', (req, res) => {
+    //sending a file
+    const filePath = path.join(import.meta.dirname, "public", "index.html") //import.meta.dirname will give directory absolute path
+    res.sendFile(filePath)
+})
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+})
