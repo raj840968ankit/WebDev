4d29796e53a79e87ea4160abf68896b10532ad0e
ankit <rajankit840968@gmail.com>
2025-06-02 04:10:27 +0530

    feat: Initialize URL Shortener project with basic structure and functionality
    - Added package.json for project dependencies and scripts
    - Created initial database migration for shortLink table
    - Set up Prisma schema for database connection and model definition
    - Implemented basic styling in style.css for the application
    - Developed routes for URL shortening functionality
    - Created services for handling database operations related to short links
    - Added EJS views for rendering the main interface and report
    - Included a header template for consistent navigation across pages

---
+++ Databases/6.Drizzle/URLShortener/app.js
@@ -0,0 +1,31 @@
+import path from 'path'
+import express from 'express'
+import { shortenerRouter } from './routes/shortener.routes.js'
+import {env} from './config/env.js'
+
+
+const app = express()
+
+const appRoot = import.meta.dirname;
+
+//serving static file to the server
+app.use(express.static(path.join(appRoot, "public")))
+
+app.use(express.urlencoded({extended : true}))  //parses post request body
+
+
+app.set('views', path.join(appRoot, "views"))   //manually give path to ejs files(dynamic html)
+app.set('view engine', 'ejs')  //using template engine(dynamic html)
+
+
+app.use((req, res, next) => {    //this middleware executes first then give access to route
+    req.appRoot = appRoot
+    next();
+});
+app.use(shortenerRouter)   //using router
+
+
+
+app.listen(env.PORT, () => {
+    console.log(`Server running at http://localhost:${env.PORT}`)
+})
---
+++ Databases/6.Drizzle/URLShortener/controllers/post.controller.js
@@ -0,0 +1,74 @@
+import crypto from 'crypto'
+import { saveLinks, loadLinks, getLinksByShortcode } from '../models/data.model.js';
+
+const postUrlShortener = async (req, res) => {
+    try {
+        const {url, shortCode} = req.body;
+        const links = await loadLinks()
+        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex")
+        //console.log(links);
+        
+        //checking in file if shortCode exists
+        // if(links[finalShortCode]){
+        //     return res.status(400).send("Short code already exists, Please choose another")
+        // }
+        links.forEach(({url, shortCode}) => {
+            if(shortCode){
+                return res.status(400).send("Short code already exists, Please choose another")
+            }
+        })
+
+        //if not exists then give value of url to finalShortCode
+        // links[finalShortCode] = url
+        // await saveLinks(links)
+
+        await saveLinks({finalShortCode, url}) 
+        return res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getReport = (req,res) => {
+    const student = [{name : 'Ankit', grade : "5th", favoriteSubject : 'Science'},
+        {name : 'Ishita', grade : "5th", favoriteSubject : 'History'},
+        {name : 'Rohan', grade : "9th", favoriteSubject : 'Biology'},
+        {name : 'Kabir', grade : "8th", favoriteSubject : 'Chemistry'},
+        {name : 'Gaurav', grade : "10th", favoriteSubject : 'Physics'}
+    ];
+    res.render("report", { student })
+}
+
+const getUrlShortener = async (req, res) => {
+  try {
+    const links = await loadLinks();
+    res.render('index', { links, req });  // passing req so you can use req.headers.host in ejs
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+}
+
+const getShortLink = async (req, res) => {
+    try {
+        const {shortCode} = req.params
+        // const links = await loadLinks()
+        //checking in database existence of url
+        const link = await getLinksByShortcode(shortCode)
+        console.log(link);
+    
+        // if(!links[shortCode]){
+        //     return res.status(404).send("404 error occurred")
+        // }
+
+        if(!link.shortCode){
+            return res.redirect('/404')
+        }
+        return res.redirect(link.url)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Internal server error")
+    }
+}
+//exporting this function to use in shortener.routes.js
+export { postUrlShortener, getReport, getUrlShortener, getShortLink };
