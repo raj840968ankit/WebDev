a187f42673cd7b215e5ab6a95cf54965ae797418
ankit <rajankit840968@gmail.com>
2025-06-16 19:05:38 +0530

    feat: Initialize React project with Vite and ESLint configuration
    - Added ESLint configuration for JavaScript and React.
    - Created index.html for the React application entry point.
    - Set up package.json with necessary dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the main application.
    - Developed App.jsx component with basic functionality and layout.
    - Added React logo SVG to assets.
    - Created index.css for global styles.
    - Implemented main.jsx as the entry point for rendering the React application.
    - Configured Vite with react plugin in vite.config.js.
    - Removed unused CSS files from the react-thapa project.
    - Updated HowNotToFetchApi component to demonstrate proper API fetching with async/await.
    - Refactored CSS styles in various components for better organization and clarity.

--- ReactJS/react-thapa/src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ ReactJS/react-thapa/src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -1,13 +1,107 @@
-import './pokemon.css'
+import { useEffect, useState } from "react";
+import "./pokemon.css";
 
-export const HowNotToFetchApi = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-    const data = res.json()
-    console.log(data);
-    
+export const HowNotToFetchApi = () => {
+    // const [apiData, setApiData] = useState([])
+
+    const [pokemon, setPokemon] = useState(null);
+    const [loading, setLoading] = useState(true); //?handling loading via fetching api
+    const [error, setError] = useState(null)
+
+    //! this way we cannot fetch API
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //     .then((res) => res.json())
+    //     .then((data) => setApiData(data))
+    //     .catch((error) => console.error(error))
+
+    //!this is the right way to fetch API
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/posts")
+    //         .then((res) => res.json())
+    //         .then((data) => setApiData(data))
+    //         .catch((error) => console.error(error))
+    // }, [])
+
+    const API = "https://pokeapi.co/api/v2/pokemon/squirtle";
+
+    // const pokemonAPI = () => {        //?handling promises with then-catch
+    //     fetch(API)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setPokemon(data);
+    //             setLoading(() => false);
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //             setLoading(() => false);
+    //         });
+    // };
+
+    const fetchPokemon = async () => {       //?handling promises with async-await
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+            setPokemon(data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setError(error);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        // pokemonAPI();
+        fetchPokemon();
+    }, []);
+
+    //!fetching api takes time so initial value will be null itself
+    //?return data only if api is fetched
+    if (loading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
+
+    if(error){
+        return (
+            <div>
+                <h1>Error : {error.message}</h1>
+            </div>
+        );
+    }
+
     return (
-        <div className="container effect-conta">
-            <ul>data : </ul>
-        </div>
-    )
-}
+        <section className="container">
+            <header>
+                <h1>Let's Catch Pokemon</h1>
+            </header>
+            <ul className="card-demo">
+                <li className="pokemon-card">
+                    <figure>
+                        <img
+                            src={pokemon.sprites.other.dream_world.front_default}
+                            alt="nn"
+                            className="pokemon-image"
+                        />
+                    </figure>
+                    <h1>{pokemon.name}</h1>
+                    <div className="grid-three-cols">
+                        <p className="pokemon-info">
+                            Height : <span>{pokemon.height}</span>
+                        </p>
+                        <p className="pokemon-info">
+                            Weight : <span>{pokemon.weight}</span>
+                        </p>
+
+                        <p className="pokemon-info">
+                            Speed : <span>{pokemon.stats[5].base_stat}</span>
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    );
+};
