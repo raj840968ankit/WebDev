5ba26b8f5ab498cee56fc3ba8d4097534d03b8ac
ankit <rajankit840968@gmail.com>
2025-06-21 00:16:32 +0530

    feat: implement WebSocket server and client connection with message handling

--- WebSocket/2.LearningSocket/client/src/App.jsx
+++ WebSocket/2.LearningSocket/client/src/App.jsx
@@ -1,7 +1,48 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client'
+import './App.css'
+import { useState } from 'react';
+import { useMemo } from 'react';
 export const App = () => {
+
+  const[message, setMessage] = useState('')
+
+  const socket = useMemo(() => io('http://localhost:3000'), []);   //!creating socket connection with backend
+
+  useEffect(() => {
+    //! listen after establishing connection
+    // socket.on('connect', () => {
+    //   console.log('connected', socket.id);
+    // })
+
+    //!listening welcome message from server
+    socket.on('welcome', (message) => {
+      console.log(message);
+    })
+
+    //!disconnect from server
+    return () => socket.disconnect()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const messageTrim = message.trim()
+    if(messageTrim){                     //!sending message to server after form submission
+      socket.emit('message', messageTrim)
+      setMessage('')
+    }
+  }
+
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div className="container">
+      <h1>Socket IO</h1>
+      <div className="chat-container">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <button type='submit'>send</button>
+        </form>
+      </div>
+    </div>
   )
 }
--- WebSocket/2.LearningSocket/server/app.js
+++ WebSocket/2.LearningSocket/server/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import {Server} from 'socket.io'
-import {createServer} from 'http'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import cors from 'cors'
 
 const PORT = 3000
 
@@ -8,17 +9,54 @@ const app = express()
 
 const server = createServer(app)   //! created server of http
 
-const io = new Server(server)    //! created io instance
+const io = new Server(server, {    //! created io server instance and client connection
+    cors: {
+        origin: 'http://localhost:5173',    //frontend URL
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true
+    }
+})
+
+
+//!.... VVI (for normal frontend and backend connection)
+app.use(cors({
+    origin: 'http://localhost:5173',    //frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>hello world</h1>`)
 })
 
-//!creating io circuit here
+
+
+//!creating io circuit here so that multiple client can connect
 io.on('connection', (socket) => {
-    console.log('user connected');
+    //!Whenever client connect then this will show new client id
+    console.log('user connected : ',socket.id);
+    
+    //!sending welcome message to all the clients
+    socket.emit('welcome', `Welcome to the server ${socket.id}`)
+
+    //!sending welcome message to all the clients except particular client after refreshing current client page
+    //? this is essential for client connection known by other client connect the room
+    socket.broadcast.emit('welcome', `${socket.id} joined the server`)
+
+    //! disconnect event listen when client will trigger server.disconnect() 
+    socket.on('disconnect', () => {
+        console.log(`User disconnected : ${socket.id}`);
+    })
+
+    //! listening message from client through UI
+    socket.on('message', (message) => {
+        console.log(message);
+    })
 })
 
-app.listen(PORT, () => {
+
+
+server.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
 })
